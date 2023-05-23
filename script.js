b678617ac529fbb1e976111796b7febcc629769b
Shubham Verma <verma.shubham2537@gmail.com>
2023-05-23 10:33:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 function highlight() {
     //Write your code here
-
+ let a = document.getElementsByTagName('strong');
+	for(let x=0;x<a.length;x++){
+		a[x].style.color = 'green';
+	}
 
 }
 
 
 function return_normal() {
     //Write your code here
-
-    
+  let b = document.getElementsByTagName('strong');
+	for(let x=0;x<b.length;x++){
+		b[x].style.color = 'black';
+	}
 }
